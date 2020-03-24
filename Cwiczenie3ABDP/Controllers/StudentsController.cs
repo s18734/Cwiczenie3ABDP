2e1f2748d4ed67bd72ccd72abf8afd6b4b8cf329
Julian Mikołajczyk <s18734@pjwstk.edu.pl>
2020-03-24 21:19:18 +0100

    Mati

--- Cwiczenie3ABDP/Controllers/StudentsController.cs
+++ Cwiczenie3ABDP/Controllers/StudentsController.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Data.SqlClient;
 using System.Linq;
 using System.Threading.Tasks;
 using Cwiczenie3ABDP.DAL;
@@ -12,7 +13,7 @@ namespace Cwiczenie3ABDP.Controllers
 {
     [Route("api/students")]
     [ApiController]
-    
+
     public class StudentsController : ControllerBase
     {
 
@@ -22,14 +23,85 @@ namespace Cwiczenie3ABDP.Controllers
 
         public StudentsController(IDbService dbService)
         {
-            _dbService =dbService;
+            _dbService = dbService;
         }
-       
+
         [HttpGet]
         public IActionResult GetStudent()
         {
-            return Ok(_dbService.GetStudents());
+            List<Object> list = new List<object>();
+            using (var client = new SqlConnection("Data Source=db-mssql;Initial Catalog=s18734;Integrated Security=True"))
+            using (var com = new SqlCommand())
+            {
+                com.Connection = client;
+                com.CommandText = "select a.FirstName, a.LastName, a.BirthDate, b.Semester, c.Name" +
+                    "               from Student a inner join Enrollment b on a.IdEnrollment = b.IdEnrollment" +
+                    "                              inner join Studies c on b.IdStudy = c.IdStudy";
+
+                client.Open();
+                var dr = com.ExecuteReader();
+                while (dr.Read())
+                {
+
+                    var st = new
+                    {
+                        FirstName = dr["FirstName"].ToString(),
+                        LastName = dr["LastName"].ToString(),
+                        BirthDate = DateTime.Parse(dr["BirthDate"].ToString()),
+                        Semester = dr["Semester"].ToString(),
+                        Name = dr["Name"].ToString()
+                    };
+                    list.Add(st);
+
+                    //...
+                }
+
+            }
+            return Ok(list);
+
         }
+        [HttpGet("{id}")]
+        public IActionResult GetStudent(string Id)
+        {
+            List<Object> list = new List<object>();
+            using (var client = new SqlConnection("Data Source=db-mssql;Initial Catalog=s18734;Integrated Security=True"))
+            using (var com = new SqlCommand())
+            {
+                com.Connection = client;
+                com.CommandText = "select a.FirstName, a.LastName, a.BirthDate, b.Semester, c.Name" +
+                    "               from Student a inner join Enrollment b on a.IdEnrollment = b.IdEnrollment" +
+                    "                              inner join Studies c on b.IdStudy = c.IdStudy where" +
+                    "                              a.IndexNumber = @id";
+                com.Parameters.AddWithValue("id", Id);
+
+                client.Open();
+                var dr = com.ExecuteReader();
+                while (dr.Read())
+                {
+
+                    var st = new
+                    {
+                        FirstName = dr["FirstName"].ToString(),
+                        LastName = dr["LastName"].ToString(),
+                        BirthDate = DateTime.Parse(dr["BirthDate"].ToString()),
+                        Semester = dr["Semester"].ToString(),
+                        Name = dr["Name"].ToString()
+                    };
+                    list.Add(st);
+
+                  
+                }
+
+            }
+            return Ok(list);
+
+        }
+
+        private void Open()
+        {
+            throw new NotImplementedException();
+        }
+
         [HttpPost]
         public IActionResult CreateStudent(Student Student)
         {
